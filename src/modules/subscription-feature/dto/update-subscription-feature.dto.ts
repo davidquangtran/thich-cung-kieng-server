d78878800e0df7bd3c884f57daf527897fa539ca
DavidQuang <tranngvietquang04@gmail.com>
2025-10-07 17:25:49 +0700

    fix: update import for PartialType and enhance update method with proper decorators in subscription feature controller

--- src/modules/subscription-feature/dto/update-subscription-feature.dto.ts
+++ src/modules/subscription-feature/dto/update-subscription-feature.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateSubscriptionFeatureDto } from './create-subscription-feature.dto';
 
 export class UpdateSubscriptionFeatureDto extends PartialType(CreateSubscriptionFeatureDto) {}
