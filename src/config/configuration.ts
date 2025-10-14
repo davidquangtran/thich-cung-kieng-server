ace505570ac957e3c77605531b2fb1c898e35418
David Quang <154660765+DavidQuang-dev@users.noreply.github.com>
2025-10-15 02:45:12 +0700

    feat: integrate Google Calendar API with event management capabilities

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -76,6 +76,24 @@ export default () => ({
     callbackUrl: process.env.GOOGLE_CALLBACK_URL,
   },
 
+  // Google Calendar
+  googleCalendar: {
+    type: process.env.GOOGLE_TYPE ?? '',
+    projectId: process.env.GOOGLE_PROJECT_ID ?? '',
+    privateKeyId: process.env.GOOGLE_PRIVATE_KEY_ID ?? '',
+    privateKey:
+      process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/"/g, '') ??
+      '',
+    clientEmail: process.env.GOOGLE_CLIENT_EMAIL ?? '',
+    clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+    authUri: process.env.GOOGLE_AUTH_URI ?? '',
+    tokenUri: process.env.GOOGLE_TOKEN_URI ?? '',
+    authProviderX509CertUrl:
+      process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL ?? '',
+    clientX509CertUrl: process.env.GOOGLE_CLIENT_X509_CERT_URL ?? '',
+    universeDomain: process.env.GOOGLE_UNIVERSE_DOMAIN ?? '',
+  },
+
   // PayOS configuration
   payos: {
     clientId: process.env.PAYOS_CLIENT_ID,
