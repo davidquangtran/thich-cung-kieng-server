c18051fe685af039aaa8dcc010bda32343790501
kat-minh <minh2k112003@gmail.com>
2025-10-29 00:29:40 +0700

    feat: add AdminStatsController for comprehensive admin dashboard statistics

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,10 +4,20 @@ import { RedisModule } from 'src/shared/redis/redis.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
+import { AdminStatsController } from './admin-stats.controller';
+import { Payment } from '../payment/entities/payment.entity';
+import { UserSubscription } from '../user-subscription/entities/user-subscription.entity';
+import { Ritual } from '../ritual/entities/ritual.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User], 'postgresql'), RedisModule],
-  controllers: [UserController],
+  imports: [
+    TypeOrmModule.forFeature(
+      [User, Payment, UserSubscription, Ritual],
+      'postgresql',
+    ),
+    RedisModule,
+  ],
+  controllers: [UserController, AdminStatsController],
   providers: [UserService],
   exports: [UserService],
 })
